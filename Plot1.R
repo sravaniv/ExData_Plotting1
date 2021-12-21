33827deafd747dc486c151895ab8b68658a4e8ec
sravaniv <svadlam1@asu.edu>
2021-12-20 21:54:37 -0800

    Added assignment solutions
    R code to generate plots and the png files of generated plots.

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+#read the data
+data<-read.table("household_power_consumption.txt", header=TRUE, sep =";",stringsAsFactors = FALSE,dec="."  )
+#subset the data to include Feb 1 and Feb 2 2007 only
+data_sub <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+#Transform Date and Time variables to Date/Time classes in R
+data_sub$Date <- as.Date(data_sub$Date,  "d/%m/%Y")
+data_sub$Time<-strptime(data_sub$Time, "%H:%M:%S")
+data_sub[1:1440, "Time"]<-format(data_sub[1:1440, "Time"], "2007-02-01 %H:%M:%S")
+data_sub[1441:2880, "Time"]<-format(data_sub[1441:2880, "Time"], "2007-02-02 %H:%M:%S")
+data_sub$Global_active_power<-as.numeric(data_sub$Global_active_power)
+data_sub$Sub_metering_1 <-as.numeric(data_sub$Sub_metering_1)
+data_sub$Sub_metering_2 <-as.numeric(data_sub$Sub_metering_2)
+data_sub$Sub_metering_3 <-as.numeric(data_sub$Sub_metering_3)
+data_sub$Voltage <-as.numeric(data_sub$Voltage)
+data_sub$Global_reactive_power <-as.numeric(data_sub$Global_reactive_power)
+#Plot 1
+png("plot1.png", width = 480, height = 480)
+hist(data_sub$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
---
+++ Plot4.R
@@ -0,0 +1,28 @@
+#read the data
+data<-read.table("household_power_consumption.txt", header=TRUE, sep =";",stringsAsFactors = FALSE,dec="."  )
+#subset the data to include Feb 1 and Feb 2 2007 only
+data_sub <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+#Transform Date and Time variables to Date/Time classes in R
+data_sub$Date <- as.Date(data_sub$Date,  "d/%m/%Y")
+data_sub$Time<-strptime(data_sub$Time, "%H:%M:%S")
+data_sub[1:1440, "Time"]<-format(data_sub[1:1440, "Time"], "2007-02-01 %H:%M:%S")
+data_sub[1441:2880, "Time"]<-format(data_sub[1441:2880, "Time"], "2007-02-02 %H:%M:%S")
+data_sub$Global_active_power<-as.numeric(data_sub$Global_active_power)
+data_sub$Sub_metering_1 <-as.numeric(data_sub$Sub_metering_1)
+data_sub$Sub_metering_2 <-as.numeric(data_sub$Sub_metering_2)
+data_sub$Sub_metering_3 <-as.numeric(data_sub$Sub_metering_3)
+data_sub$Voltage <-as.numeric(data_sub$Voltage)
+data_sub$Global_reactive_power <-as.numeric(data_sub$Global_reactive_power)
+#Plot 4
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+with(data_sub, {
+  plot(data_sub$Time, data_sub$Global_active_power, type="l", ylab = "Global Active Power (kilowatts)",xlab = "Time", main = "Power vs Time")
+  plot(data_sub$Time, data_sub$Voltage, type="l", ylab = "Voltage",xlab = "datetime", main = "Voltage vs Time")
+  plot(data_sub$Time,data_sub$Sub_metering_1, type="n", xlab="Time", ylab = "Energy sub metering", main = "Energy Sub Metering Vs. Time")
+  with(data_sub, lines(Time,Sub_metering_1))
+  with(data_sub, lines(Time,Sub_metering_2, col="red"))
+  with(data_sub, lines(Time,Sub_metering_3, col="blue"))
+  legend("topright",lty=1, col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(data_sub$Time, data_sub$Global_reactive_power, type="l", ylab = "Global_reactive_power",xlab = "datetime", main = "Global Reactive Power vs Time")
+})
